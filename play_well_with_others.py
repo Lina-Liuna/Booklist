ed87c20fedada86672e6fb9834f533715df9f9b2
Lina <liuna.lina@gmail.com>
2023-01-24 10:12:02 -0800

    Add favoriate books: play well with others

---
+++ play_well_with_others.py
@@ -0,0 +1,27 @@
+import collections
+
+Intro_words = collections.defaultdict(list)
+Intro_words = {
+    'As the negotiator starts talking with him, I quiz myself on proper procedure:':
+    [
+        'Slow it down',
+        'Use active listening',
+        'Voice tone is important',
+        'Remember that your actions are contagious',
+    ],
+    'Most import thing is:': [
+        'Keep them talking',
+        'Don\'t freaking out',
+    ],
+    'The skeleton key to human communication': [
+        'active listening',
+    ],
+    'George Vaillant replied:': [
+        'That the only thing taht really matters in life are your relationship to other people'
+    ],
+    'the relationship that matters:' : [
+        'Whom Can I trust?',
+        'Does anyone really know me?',
+        'Does '
+    ]
+}
