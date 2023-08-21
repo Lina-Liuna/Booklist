112a64f28ac7d2d9d890d5db4e16bb2519f2ea52
Lina <liuna.lina@gmail.com>
2023-08-20 17:45:22 -0700

    Writing Academic Practice

---
+++ TOEFL_Writing_Academic_Practice.py
@@ -0,0 +1,25 @@
+questsions_example_list = [
+    '1. The decision to do XXX can have both advantages and disadvantages for XXX',
+    '2. The impact can vary based on individual circumstances and goals',
+    '3. xxx can provide an opportunity for personal exploration, self-discovery, and growth.',
+    '4. It allows XXX to step out of their comfort zones, gain new perspective, and develop life skills',
+    '5. After years of XXXX, some XXX may feel XXX. XXX can provide a XXX that allows them to recharge, reducing stress and improving mental well-being',
+    '6. During XXX, sb can engage in internships, volunteer work, jobs, or travel.'
+    '7. This hand-on experience can enhance their understanding of various industries, cultures, and work environments',
+    '8. XXX can help XXX clarify their XXX goals. It gives them XXX(time) to reflect on their interests and passion before doing sth',
+    '9. '
+]
+
+question_topic_title_list = [
+    '1. personal growth',
+    '2. Real-World Experience',
+    '3. Clarifying Goals',
+    '4. Improved Academics',
+    '5. Academic Disruption',
+    '6. Financial Concerns',
+    '7. Delayed Graduation',
+    '8. Professional Competitiveness, XXX may not be viewed positively by potential employers, align with one\'s career aspirations',
+    '9. Lack of structure - without a clear plan, XXX could lead to unproductive or aimless time',
+    '10. It is essential for students and their families to carfully consider both ... before making a decision',
+
+]
